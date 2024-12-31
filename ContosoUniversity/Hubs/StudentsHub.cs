4e350482780407b5a56b799bce3eb56438c7b416
Leonardo Henrique da Rocha Araujo <136940515+leonardohra-gh@users.noreply.github.com>
2024-12-31 12:24:00 -0300

    Removendo coisas não usadas

--- ContosoUniversity/Hubs/StudentsHub.cs
+++ ContosoUniversity/Hubs/StudentsHub.cs
@@ -4,10 +4,6 @@ namespace ContosoUniversity.Hubs
 {
     public class StudentsHub() : Hub
     {
-        // Criar o método que deve mandar o sinal para todos os clientes
-        public async Task UpdateStudentCount(int newAmount)
-        {
-            await Clients.All.SendAsync("UpdateStudentCount", newAmount);
-        }
+
     }
 }
